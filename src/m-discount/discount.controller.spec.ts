ad2380d13e3abf03ac07ff71e26f06107d0504cc
Puria Kordrostami <kordeviant@gmail.com>
2021-11-24 00:21:34 +0330

    add simple unit test

---
+++ src/m-discount/discount.controller.spec.ts
@@ -0,0 +1,35 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AppModule } from '../app.module';
+import { startTestMango } from '../startTestMango';
+import { DiscountController } from './discount.controller';
+
+describe('DiscountController', () => {
+  let discountController: DiscountController;
+  let mongod;
+  let app: TestingModule;
+
+  beforeAll(async () => {
+    console.log('nnnnn');
+
+    mongod = await startTestMango();
+    app = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    discountController = app.get<DiscountController>(DiscountController);
+  });
+  afterAll(async () => {
+    await mongod.stop();
+    await app.close();
+  });
+
+  beforeEach(async () => {
+    console.log('asdasda222232323');
+  });
+
+  describe('root', () => {
+    it('wrong product id should return "-1"', async () => {
+      expect(await discountController.getDiscount('2')).toBe(-1);
+    });
+  });
+});
